2c19c81a21f4eb0853665a869bf711ed0d6ab10a
Angel <angel.mascarell.tf@gmail.com>
2024-11-14 16:30:15 +0100

    fix json token name, new images added

--- src/main/java/com/collectorhub/collectorhub/controller/response/AuthResponse.java
+++ src/main/java/com/collectorhub/collectorhub/controller/response/AuthResponse.java
@@ -1,6 +1,7 @@
 package com.collectorhub.collectorhub.controller.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,7 @@ import org.springframework.validation.annotation.Validated;
 @Builder
 public class AuthResponse {
 
+    @JsonProperty(value = "accessToken")
     private String token;
 
 }
